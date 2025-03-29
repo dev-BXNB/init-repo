386aa77e6047078a6dd55c6ff996178f770887c8
reblixane <reblixane@gmail.com>
2025-03-29 11:40:14 -0700

    Updated the landscape mode for mobile devices

--- Cold3X.com/script.js
+++ Cold3X.com/script.js
@@ -1,17 +1,27 @@
+// Script for back to top button
 let scrollMain = document.getElementById("scrollMe");
 
 
 
 let topBtn = document.getElementById("top");
 
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll", () => {
   let scrollDistance = window.scrollY;
-  if(scrollDistance > 100){
+  if (scrollDistance > 100) {
     console.log(scrollDistance);
     topBtn.style.display = "block";
   }
 
-  else if(scrollDistance < 100){
+  else if (scrollDistance < 100) {
     topBtn.style.display = "none";
   }
+});
+// End of back to top button script
+
+// Script to scroll main on window scroll for pc 
+
+let main = document.getElementById("scrollMe");
+
+window.addEventListener("wheel", (e) => {
+  main.scrollTop += e.deltaY;
 });
