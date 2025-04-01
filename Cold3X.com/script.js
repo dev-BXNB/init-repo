d2074e99f4affcd9256aa8d04039111b7e08aa33
reblixane <reblixane@gmail.com>
2025-04-01 03:42:38 -0700

    added a few projects

--- Cold3X.com/script.js
+++ Cold3X.com/script.js
@@ -24,7 +24,7 @@ window.addEventListener("scroll", () => {
 let main = document.getElementById("scrollMe");
 
 window.addEventListener("wheel", (e) => {
-  main.scrollTop += e.deltaY;
+  main.scrollTop += 100;
 });
 
 // End of script 
