e7ce0952c7a0ec553d90ecdeee6d7b3188d7809c
reblixane <reblixane@gmail.com>
2025-04-07 22:15:40 -0700

    Revert "added a mouse tracking feature"
    This reverts commit 127947daa897f1ba263aad2011ea65ef2ac64c28.

--- Cold3X.com/script.js
+++ Cold3X.com/script.js
@@ -21,24 +21,11 @@ window.addEventListener("wheel", (e) => {
   main.scrollBy(0, (e.deltaY * 2.5));
 });
 
-let mouseMove = document.getElementById("mouseMove");
-
-window.addEventListener("mousemove", (e) => {
-  let xPos = e.clientX; // Correct: Use clientX
-  let yPos = e.clientY; // Correct: Use clientY
-
-  // Adjust the position to center the element on the cursor
-  // (Otherwise, the top-left corner will be at the cursor)
-  const elementWidth = mouseMove.offsetWidth;
-  const elementHeight = mouseMove.offsetHeight;
-  const cursorX = xPos - elementWidth / 2;
-  const cursorY = yPos - elementHeight / 2;
-
-  mouseMove.style.left = `${cursorX}px`; // Use template literals and add "px"
-  mouseMove.style.top = `${cursorY}px`; // Use template literals and add "px"
-  mouseMove.style.position = "fixed";
-  mouseMove.style.pointerEvents = "none";
-});
+window.addEventListener("mousemove", (e)=>{
+  let xPos = window.screenX;
+  let yPos = window.screenY;
+  
+})
 
 
 
